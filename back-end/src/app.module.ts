4d2a7ef9d5d23fa796310bf404a9de1184e0c60f
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-06-25 20:56:16 +0300

    f

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Ddg@36240667',
-      database: 'gore_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities:true,
       synchronize:true,
     }),
