3e0661bb5dc55c4305ef795cd11606ba3f338852
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-06-27 12:07:02 +0300

    refactor: enhance CreateAdminDto with validation decorators

--- server/src/admin/dto/create-admin.dto.ts
+++ server/src/admin/dto/create-admin.dto.ts
@@ -1,5 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateAdminDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   fullName!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(150)
   email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   password!: string;
 }
