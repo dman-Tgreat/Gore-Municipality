3cf70196a83bf33bdaa8fe0065960fa57a6cd4d3
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-06-27 15:35:39 +0300

    fixed the cors for the connection b/n client and server

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
